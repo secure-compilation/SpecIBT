9b83d438958924f81d38c090e17b7a70d68c3b9e
julayleathermanbrooks <julaybrooks@me.com>
2025-10-20 13:12:42 +0200

    more cleanup

--- MiniCET_Index.v
+++ MiniCET_Index.v
@@ -19,37 +19,6 @@ Set Default Goal Selector "!".
 
 (** Sequential small-step semantics for MiniCET *)
 
-(* Definition step (p:prog) (c:cfg) : option (cfg * obs) :=
-  let '(pc, r, m, sk) := c in
-  i <- p[[pc]];;
-  match i with
-  | <{{skip}}> | <{{ctarget}}> =>
-      ret ((pc+1, r, m, sk), [])
-  | <{{x:=e}}> =>
-      ret ((pc+1, (x !-> eval r e; r), m, sk), [])
-  | <{{branch e to l}}> =>
-      n <- to_nat (eval r e);;
-      let b := not_zero n in
-      ret ((if b then (l,0) else pc+1, r, m, sk), [OBranch b])
-  | <{{jump l}}> =>
-      ret (((l,0), r, m, sk), [])
-  | <{{x<-load[e]}}> =>
-      n <- to_nat (eval r e);;
-      v' <- nth_error m n;;
-      ret ((pc+1, (x !-> v'; r), m, sk), [OLoad n])      
-  | <{{store[e]<-e'}}> =>
-      n <- to_nat (eval r e);;
-      ret ((pc+1, r, upd n m (eval r e'), sk), [OStore n])
-  | <{{call e}}> =>
-      l <- to_fp (eval r e);;
-      ret (((l,0), r, m, (pc+1)::sk), [OCall l])
-  | <{{ret}}> =>
-      pc' <- hd_error sk;;
-      ret ((pc', r, m, tl sk), [])
-  end. 
-
- *)
-
 Reserved Notation
   "p '|-' '<((' c '))>' '-->^' os '<((' ct '))>'"
   (at level 40, c constr, ct constr).
@@ -92,6 +61,8 @@ Inductive seq_eval_small_step_inst (p:prog) :
   where "p |- <(( c ))> -->^ os <(( ct ))>" :=
       (seq_eval_small_step_inst p c ct os).
 
+(** Sequential multi-step relation *)
+
 Reserved Notation 
   "p '|-' '<((' c '))>' '-->*^' os '<((' ct '))>'"
       (at level 40, c constr, ct constr).
@@ -106,69 +77,7 @@ Inductive multi_seq_inst (p : prog) (c : cfg) : cfg -> obs -> Prop :=
   where "p |- <(( c ))> -->*^ os <(( ct ))>" :=
       (multi_seq_inst p c ct os).
 
-(* Reserved Notation *)
-(*    "p '|-' '<((' c , st , ast '))>' '-->*^' os '<((' ct , stt , astt '))>'" *)
-(*    (at level 40, c custom com at level 99, ct custom com at level 99, *)
-(*     st constr, ast constr, stt constr, astt constr at next level). *)
-
-(* Inductive multi_seq (p:prog) (c:com) (st:state) (ast:astate) : *)
-(*     com -> state -> astate -> obs -> Prop := *)
-(*   | multi_seq_refl : p |- <((c, st, ast))> -->*^[] <((c, st, ast))> *)
-(*   | multi_seq_trans (c':com) (st':state) (ast':astate) *)
-(*                 (c'':com) (st'':state) (ast'':astate) *)
-(*                 (os1 os2 : obs) : *)
-(*       p |- <((c, st, ast))> -->^os1 <((c', st', ast'))> -> *)
-(*       p |- <((c', st', ast'))> -->*^os2 <((c'', st'', ast''))> -> *)
-(*       p |- <((c, st, ast))> -->*^(os1++os2) <((c'', st'', ast''))> *)
-
-(*   where "p |- <(( c , st , ast ))> -->*^ os <(( ct ,  stt , astt ))>" := *)
-(*     (multi_seq p c st ast ct stt astt os). *)
-
-(** Small-step speculative semantics for MiniCET *)
-
-(* Definition spec_step (p:prog) (sc:spec_cfg) (ds:dirs) : option (spec_cfg * dirs * obs) := *)
-(*   let '(c, ct, ms) := sc in *)
-(*   let '(pc, r, m, sk) := c in *)
-(*   i <- p[[pc]];; *)
-(*   match i with *)
-(*   | <{{branch e to l}}> => *)
-(*       is_false ct;; *)
-(*       d <- hd_error ds;; *)
-(*       b' <- is_dbranch d;; *)
-(*       n <- to_nat (eval r e);; *)
-(*       let b := not_zero n in *)
-(*       let ms' := ms || negb (Bool.eqb b b') in  *)
-(*       let pc' := if b' then (l, 0) else (pc+1) in *)
-(*       ret ((((pc', r, m, sk), ct, ms'), tl ds), [OBranch b]) *)
-(*   | <{{call e}}> => *)
-(*       is_false ct;; *)
-(*       d <- hd_error ds;; *)
-(*       pc' <- is_dcall d;; *)
-(*       l <- to_fp (eval r e);; *)
-(*       let ms' := ms || negb ((fst pc' =? l) && (snd pc' =? 0)) in *)
-(*       ret ((((pc', r, m, (pc+1)::sk), true, ms'), tl ds), [OCall l]) *)
-(*   | <{{ctarget}}> => *)
-(*       is_true ct;; (* ctarget can only run after call? (CET) Maybe not? *) *)
-(*       ret (((pc+1, r, m, sk), false, ms), ds, []) *)
-(*   | _ => *)
-(*       is_false ct;; *)
-(*       co <- step p c;; *)
-(*       let '(c',o) := co in *)
-(*       ret ((c',false,ms), ds, o) *)
-(*   end. *)
-
-(* Fixpoint spec_steps (f:nat) (p:prog) (c:spec_cfg) (ds:dirs) *)
-(*   : option (spec_cfg * dirs * obs) := *)
-(*   match f with *)
-(*   | S f' => *)
-(*       cdo1 <- spec_step p c ds;; *)
-(*       let '(c1,ds1,o1) := cdo1 in *)
-(*       cdo2 <- spec_steps f' p c1 ds1;; *)
-(*       let '(c2,ds2,o2) := cdo2 in *)
-(*       ret (c2,ds2,o1++o2) *)
-(*   | 0 => *)
-(*       None (* Q: Do we need more precise out-of-fuel error here? *) *)
-(*   end. *)
+(** Speculative small-step semantics for MiniCET *)
 
 Reserved Notation
   "p '|-' '<((' sc '))>' '-->_' ds '^^' os '<((' sct '))>'"
@@ -216,6 +125,7 @@ Inductive spec_eval_small_step (p:prog):
   where "p |- <(( sc ))> -->_ ds ^^ os  <(( sct ))>" :=
     (spec_eval_small_step p sc sct ds os).
 
+(** Speculative multi-step relation *)
 
 Reserved Notation
   "p '|-' '<((' sc '))>' '-->*_' ds '^^' os '^^' n '<((' sct '))>'"
@@ -232,7 +142,6 @@ Inductive multi_spec_inst (p:prog) :
     (multi_spec_inst p sc sct ds os n).
 
 (** Ideal small-step semantics for MiniCET *)
-(* Source program executing speculatively with uslh protections encoded in semantics *)
 
 Reserved Notation
   "p '|-' '<((' sc '))>' '-->i_' ds '^^' os '<((' sct '))>'"
@@ -246,32 +155,32 @@ Inductive ideal_eval_small_step_inst (p:prog) :
   | ISMI_Asgn : forall pc r m sk ms e x,
       p[[pc]] = Some <{{ x := e }}> ->
       p |- <(( ((pc, r, m, sk), false, ms) ))> -->_[]^^[] <(( ((pc+1, (x !-> (eval r e); r), m, sk), false, ms) ))>
-  | ISMI_Branch : forall pc pc' r m sk (ms ms' b b' : bool) e n n' l, (* uslh protection is to mask branch condition under speculation *)
+  | ISMI_Branch : forall pc pc' r m sk (ms ms' b b' : bool) e n n' l, 
       p[[pc]] = Some <{{ branch e to l }}> ->
       to_nat (eval r e) = Some n ->
-      n' = (if ms then 0 else n) -> (* mask branch condition under speculation *)
-      b = (not_zero n') -> (* information leak uses potentially masked value *)
-      pc' = (if b' then (l,0) else pc+1) -> (* control flow uses attacker's value *)
-      ms' = (ms || (negb (Bool.eqb b b'))) -> (* set ms according to whether these are different *)
+      n' = (if ms then 0 else n) -> 
+      b = (not_zero n') -> 
+      pc' = (if b' then (l,0) else pc+1) -> 
+      ms' = (ms || (negb (Bool.eqb b b'))) -> 
       p |- <(( ((pc, r, m, sk), false, ms) ))> -->_[DBranch b']^^[OBranch b] <(( ((pc', r, m, sk), false, ms') ))>
   | ISMI_Jump : forall l pc r m sk ms,
       p[[pc]] = Some <{{ jump l }}> ->
       p |- <(( ((pc, r, m, sk), false, ms) ))> -->_[]^^[] <(( (((l,0), r, m, sk), false, ms) ))>
-  | ISMI_Load : forall pc r m sk x e n n' v' (ms : bool), (* uslh : mask addr under spec *)
+  | ISMI_Load : forall pc r m sk x e n n' v' (ms : bool), 
       p[[pc]] = Some <{{ x <- load[e] }}> ->
       to_nat (eval r e) = Some n ->
       nth_error m n = Some v' ->
-      n' = (if ms then 0 else n) -> (* mask addr under speculation, use for obs *)
+      n' = (if ms then 0 else n) -> 
       p |- <(( ((pc, r, m, sk), false, ms) ))> -->_[]^^[OLoad n'] <(( ((pc+1, (x !-> v'; r), m, sk), false, ms) ))>
   | ISMI_Store : forall pc r m sk e e' e'' n (ms : bool),
       p[[pc]] = Some <{{ store[e] <- e' }}> ->
       to_nat (eval r e) = Some n ->
-      e'' = (if ms then 0 else n) -> (* mask addr under spec, use for obs *)
+      e'' = (if ms then 0 else n) -> 
       p |- <(( ((pc, r, m, sk), false, ms) ))> -->_[]^^[OStore e''] <(( ((pc+1, r, upd n m (eval r e'), sk), false, ms) ))>
   | ISMI_Call : forall pc pc' r m sk e l l' (ms ms' : bool),
       p[[pc]] = Some <{{ call e }}> ->
       to_fp (eval r e) = Some l ->
-      l' = (if ms then 0 else l) -> (* mask fp under spec, use for obs *)
+      l' = (if ms then 0 else l) -> 
       ms' = ms || negb ((fst pc' =? l) && (snd pc' =? 0)) ->
       p |- <(( ((pc, r, m, sk), false, ms) ))> -->_[DCall pc']^^[OCall l'] <(( ((pc', r, m, (pc+1)::sk), true, ms') ))>
   | ISMI_CTarget : forall pc r m sk ms,
@@ -283,6 +192,8 @@ Inductive ideal_eval_small_step_inst (p:prog) :
 
   where "p |- <(( sc ))> -->_ ds ^^ os  <(( sct ))>" :=
     (ideal_eval_small_step_inst p sc sct ds os).
+
+(** Ideal multi-step relation *)
 
 Reserved Notation
   "p '|-' '<((' ic '))>' '-->*_' ds '^^' os '<((' ict '))>'"
