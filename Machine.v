73ea20cdb0d005b8847fe1cff87f9b8341d2920b
Yan Farba <yan.farba@gmail.com>
2025-12-16 18:37:11 +0100

    fix: adopted the new `Semantics` module definition

--- Machine.v
+++ Machine.v
@@ -192,38 +192,38 @@ Definition val_inject_s (p: prog) (vsrc vtgt: val) : Prop :=
 
 Module MachineCommon (M: TMap).
 
-Notation "'_' '!->' v" := (M.init v)
-    (at level 100, right associativity).
-Notation "x '!->' v ';' m" := (M.t_update m x v)
-    (at level 100, v at next level, right associativity).
-Notation "m '!' x" := (M.t_apply m x)
-    (at level 20, left associativity).
+  Notation "'_' '!->' v" := (M.init v)
+      (at level 100, right associativity).
+  Notation "x '!->' v ';' m" := (M.t_update m x v)
+      (at level 100, v at next level, right associativity).
+  Notation "m '!' x" := (M.t_apply m x)
+      (at level 20, left associativity).
 
-Definition reg := M.t val.
-Definition reg_init := M.init UV.
+  Definition reg := M.t val.
+  Definition reg_init := M.init UV.
 
-Definition no_fp (r: reg) : Prop :=
-  forall x, match (to_fp (r ! x)) with
-       | Some _ => False
-       | None => True
-       end.
+  Definition no_fp (r: reg) : Prop :=
+    forall x, match (to_fp (r ! x)) with
+        | Some _ => False
+        | None => True
+        end.
 
-Fixpoint eval (st : reg) (e: exp) : val :=
-  match e with
-  | ANum n => N n
-  | AId x => M.t_apply st x
-  | ABin b e1 e2 => eval_binop b (eval st e1) (eval st e2)
-  | <{b ? e1 : e2}> =>
-      match to_nat (eval st b) with (* Can't branch on function pointers *)
-      | Some n1 => if not_zero n1 then eval st e1 else eval st e2
-      | None => UV
-      end
-  | <{&l}> => N l
-  end.
+  Fixpoint eval (st : reg) (e: exp) : val :=
+    match e with
+    | ANum n => N n
+    | AId x => M.t_apply st x
+    | ABin b e1 e2 => eval_binop b (eval st e1) (eval st e2)
+    | <{b ? e1 : e2}> =>
+        match to_nat (eval st b) with (* Can't branch on function pointers *)
+        | Some n1 => if not_zero n1 then eval st e1 else eval st e2
+        | None => UV
+        end
+    | <{&l}> => N l
+    end.
 
 Definition cfg : Type := ((cptr*reg)*mem)*list cptr. (* (pc, register set, memory, stack frame) *)
 
-Definition spec_cfg : Type := ((cfg * bool) * bool).
+  Definition spec_cfg : Type := ((cfg * bool) * bool).
 
 (* ret with empty stackframe *)
 Definition final_spec_cfg (p: prog) (sc: spec_cfg) : bool :=
@@ -240,7 +240,155 @@ Definition final_spec_cfg (p: prog) (sc: spec_cfg) : bool :=
   | None => false
   end.
 
-Definition ideal_cfg : Type := (cfg * bool)%type.
+  Definition ideal_cfg : Type := (cfg * bool)%type.
+
+  Definition DIRS := Machine.dirs.
+  Definition PC := nat.
+  Definition REG := reg.
+
+  Definition fetch := flat_fetch.
+
+  Definition step (p:prog) (sc:state cfg) : (state cfg * obs) :=
+    match sc with
+    | S_Running c =>
+        let '(pc, r, m, sk) := c in
+        match flat_fetch p pc with
+        | Some i =>
+            match i with
+            | <{{skip}}> | <{{ctarget}}> =>
+              (S_Running (add pc 1, r, m, sk), [])
+            | <{{x:=e}}> =>
+              (S_Running (add pc 1, (x !-> eval r e; r), m, sk), [])
+            | <{{branch e to l}}> =>
+                match to_nat (eval r e) with
+                | Some n => let b := not_zero n in
+                          (S_Running (if b then l else add pc 1, r, m, sk), [OBranch b])
+                | None => (S_Undef, [])
+                end
+            | <{{jump l}}> =>
+              (S_Running (l, r, m, sk), [])
+            | <{{x<-load[e]}}> =>
+                match to_nat (eval r e) with
+                | Some n => match nth_error m n with
+                          | Some v' => (S_Running (add pc 1, (x !-> v'; r), m, sk), [OLoad n])
+                          | None => (S_Undef, [])
+                          end
+                | None => (S_Undef, [])
+                end
+            | <{{store[e]<-e'}}> =>
+                match to_nat (eval r e) with
+                | Some n => (S_Running (add pc 1, r, (upd n m (eval r e')), sk), [OStore n])
+                | None => (S_Undef, [])
+                end
+            | <{{call e}}> =>
+                match to_nat (eval r e) with
+                | Some l => (S_Running (l, r, m, (add pc 1)::sk), [OCall l]) (* Checking needed *)
+                | None => (S_Undef, [])
+                end
+            | <{{ret}}> =>
+              match sk with
+              | [] => (S_Term, [])
+              | pc'::stk' => (S_Running (pc', r, m, stk'), [])
+              end
+            end
+        | None => (S_Fault, [])
+        end
+    | s => (s, [])
+    end.
+
+  (* Morally state+output monad hidden in here: step p >> steps f' p  *)
+  Fixpoint steps (f:nat) (p:prog) (sc: state cfg) : (state cfg * obs) :=
+    match f with
+    | S f' =>
+        match sc with
+        | S_Running c =>
+            let '(c1, o1) := step p sc in
+            let '(c2, o2) := steps f' p c1 in
+            (c2, o1++o2)
+        | s => (s, [])
+        end
+    | 0 =>
+        (sc, [])
+    end.
+
+  Definition spec_step (p:prog) (ssc: state spec_cfg) (ds: DIRS) : (state spec_cfg * DIRS * obs) :=
+    match ssc with
+    | S_Running sc =>
+        let '(c, ct, ms) := sc in
+        let '(pc, r, m, sk) := c in
+        match flat_fetch p pc with
+        | None => untrace "lookup fail" (S_Undef, ds, [])
+        | Some i =>
+            match i with
+            | <{{branch e to l}}> =>
+              if ct then (*untrace "ct set at branch"*) (S_Fault, ds, []) else
+              match
+                if seq.nilp ds then
+                  untrace "Branch: Directions are empty!" None
+                else
+                  d <- hd_error ds;;
+                  b' <- is_dbranch d;;
+                  n <- to_nat (eval r e);;
+                  let b := not_zero n in
+                  let ms' := ms || negb (Bool.eqb b b') in
+                  let pc' := if b' then l else (add pc 1) in
+                  ret ((S_Running ((pc', r, m, sk), ct, ms'), tl ds), [OBranch b])
+              with
+              | None => untrace "branch fail" (S_Undef, ds, [])
+              | Some (c, ds, os) => (c, ds, os)
+              end
+            | <{{call e}}> =>
+              if ct then (*untrace "ct set at call"*) (S_Fault, ds, []) else
+              match
+                if seq.nilp ds then
+                  untrace "Call: Directions are empty!" None
+                else
+                  d <- hd_error ds;;
+                  pc' <- is_dcall d;;
+                  l <- to_nat (eval r e);;
+                  let ms' := ms || negb ((pc' =? l)%nat) in
+                  ret ((S_Running ((pc', r, m, (add pc 1)::sk), true, ms'), tl ds), [OCall l])
+              with
+              | None => untrace "call fail" (S_Undef, ds, [])
+              | Some (c, ds, os) => (c, ds, os)
+              end
+            | <{{ctarget}}> =>
+              match
+                is_true ct;;
+                (ret (S_Running ((add pc 1, r, m, sk), false, ms), ds, []))
+              with
+              | None => untrace "ctarget fail!" (S_Undef, ds, [])
+              | Some (c, ds, os) => (c, ds, os)
+              end
+            | _ =>
+              if ct then (*untrace ("ct set at " ++ show i)*) (S_Fault, ds, [])
+              else
+                match step p (S_Running c) with
+                | (S_Running c', o) => (S_Running (c', false, ms), ds, o)
+                | (S_Undef, o) => (S_Undef, ds, o)
+                | (S_Fault, o) => (S_Fault, ds, o)
+                | (S_Term, o) => (S_Term, ds, o)
+                end
+            end
+        end
+    | s => (s, ds, [])
+    end.
+
+  Fixpoint spec_steps (f:nat) (p:prog) (sc: state spec_cfg) (ds:DIRS)
+    : (state spec_cfg * DIRS * obs) :=
+    match f with
+    | S f' =>
+        match sc with
+        | S_Running c =>
+            let '(c1,ds1,o1) := spec_step p sc ds in
+            let '(c2,ds2,o2) := spec_steps f' p c1 ds1 in
+            (c2,ds2,o1++o2)
+        | s => (s, ds, [])
+        end
+    | 0 =>
+        (sc, ds, []) (* JB: executing for 0 steps should be just the identity... *)
+        (* None *) (* Q: Do we need more precise out-of-fuel error here? *)
+    end.
 
 End MachineCommon.
 
